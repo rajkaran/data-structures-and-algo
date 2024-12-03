56011966368896a39864e874d44ba58ca595d9d0
Rajkaran <rajkaran.chauhan07@gmail.com>
2024-12-02 20:25:07 -0500

    create better palidrome solution

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -14,17 +14,36 @@ function checkPalindrome(str) {
     return (clone.join('') == sarr.join(''));
 }
 
-console.log(checkPalindrome('awesome')); // false
-console.log(checkPalindrome('foobar')); // false
-console.log(checkPalindrome('tacocat')); // true
-console.log(checkPalindrome('amanaplanacanalpanama')); // true
 
-const status1 = checkPalindrome('A man, a plan, a canal. Panama');
-console.log('is "A man, a plan, a canal. Panama" palindrome ', status1);
+// O(n/2) simplifies to O(n)
+function checkPalindrome1(str) {
+    let clean = str.replace(/[^a-zA-z]/gi, '');
+    let firstIndex = 0;
+    let lastIndex = clean.length - 1;
+
+    while(firstIndex !== lastIndex){
+        if(clean[firstIndex] !== clean[lastIndex]){
+            return false
+        }
+
+        firstIndex++;
+        lastIndex--;
+    }
+  
+    return true;
+}
+
+console.log(checkPalindrome1('awesome')); // false
+console.log(checkPalindrome1('foobar')); // false
+console.log(checkPalindrome1('tacocat')); // true
+console.log(checkPalindrome1('amanaplanacanalpanama')); // true
+
+// const status1 = checkPalindrome('A man, a plan, a canal. Panama');
+// console.log('is "A man, a plan, a canal. Panama" palindrome ', status1);
 
 
-const status2 = checkPalindrome('almostomla');
-console.log('is "almostomla" palindrome ', status2);
+// const status2 = checkPalindrome('almostomla');
+// console.log('is "almostomla" palindrome ', status2);
 
-const status3 = checkPalindrome('anna');
-console.log('is "anna" palindrome ', status3);
+// const status3 = checkPalindrome('anna');
+// console.log('is "anna" palindrome ', status3);
